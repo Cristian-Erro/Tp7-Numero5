51d40717028cf8538972fc2bd47aa4e786c1945a
Cristian Erro <cristianlp86@gmail.com>
2022-10-06 23:14:35 -0300

    agregue comentarios

--- Program.cs
+++ Program.cs
@@ -6,24 +6,26 @@ namespace Numero5
     {
         static void Main(string[] args)
         {
+            //Vaariables
             int nro;
             int resultado;
-
+            //Bienvenida
             Console.Clear();
             Console.ForegroundColor = ConsoleColor.Cyan;
             Console.WriteLine("--Bienvenido al sistema de factoriales---");
-
+            //Entrada
             Console.WriteLine("Ingrese el numero a calcular su factorial");
             nro = Convert.ToInt32(Console.ReadLine());
             Console.Clear();
             
-           
+            //Llamo a la funcion
             resultado=factorial(nro);
-            
+            //Resultado
             Console.WriteLine("El factorial del numero " + nro + " es " +resultado);
             Console.WriteLine("----Gracias por utilizar mi programa!----");
             
         }
+            //Funcion que calcula factorial
         static int factorial(int nro)
         {
             int facto=1;
